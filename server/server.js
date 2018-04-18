2bb093a6a3d1da585741483f6fae1af7cd8c3875
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-04-18 22:39:14 +0530

    Added express

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/client/index.html'));
+});
+
+app.listen(4000, () => {
+  console.log('Listening');
+});
