79595013fc14917a0ef5a02fd9629530b08be6b6
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-04-19 11:27:44 +0530

    Added vue-router. Closes #1

--- client/src/app.js
+++ client/src/app.js
@@ -3,8 +3,12 @@ import App from './App.vue';
 
 // Vuetify
 import Vuetify from 'vuetify';
+
+// Vue-router
+import VueRouter from 'vue-router';
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
 /* eslint-disable-next-line no-new */
 new Vue({
