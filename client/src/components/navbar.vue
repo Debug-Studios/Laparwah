0b074efb5ef46f6194335cb2a41913ef4541b9ec
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-04-21 11:44:59 +0530

    Removed manual login options, added social networks login.
    Kyuuuuuuuuuu @prajjwaldimri @ayushpant @HURRICANEE

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -5,47 +5,48 @@
         span.hidden-sm-and-down Laparwah
       v-text-field.hidden-sm-and-down(flat='' solo-inverted='' prepend-icon='search' label='Search')
       v-spacer
-      v-btn(icon='' @click.stop='dialog = !dialog')
-        v-icon account_circle
+      v-menu(offset-y='')
+        v-btn(icon='' slot='activator')
+          v-icon account_circle
+        v-card
+          v-flex.text-xs-center(xs12)
+            span(style='margin-top:30px;') SIGN IN USING
+          v-flex.text-xs-center(xs12 )
+            a(href='/auth/google')
+              v-avatar(size='32')
+                img(src='/icons/google.svg')
+            a(href='/auth/facebook')
+              v-avatar(size='32')
+                img(src='/icons/facebook.svg')
+            a(href='/auth/twitter')
+              v-avatar(size='32')
+                img(src='/icons/twitter.svg')
+            a(href='/auth/microsoft')
+              v-avatar(size='32')
+                img(src='/icons/windows.svg')
+            
+
+          
+      
+
     v-content
       v-container(fluid='' fill-height='')
         v-layout(justify-center='' align-center='')
     v-btn(fab='' bottom='' right='' color='pink' dark='' fixed='' @click.stop='')
       v-icon keyboard_arrow_up
-    v-dialog(v-model='dialog' width='400px')
-      v-card
-        v-card-title.gray.lighten-4.py-4.title
-          | SIGN IN
-        v-container.pa-4(grid-list-sm='')
-          v-layout(row='' wrap='')
-            v-flex(xs12='')
-              v-text-field(v-model='email' prepend-icon='mail' label='E-mail' :error-messages="errors.collect('email')" v-validate="'required|email'" data-vv-name='email' required='')
-            v-flex(xs12='')
-              v-text-field(v-model='password' prepend-icon='lock' label='Password' :type="'password'" required='')
-            v-flex.text-xs-center(xs12='')
-              v-btn(color='info' :loading='loading4' @click.native="loader = 'loading4'" :disabled='loading4' @click='submit')
-                | Sign in
-                span.custom-loader(slot='loader')
-                  v-icon(light='') cached
-              router-view
-            v-flex.text-xs-center(xs12='')
-              span  OR 
-            v-flex.text-xs-center(xs12='')
-              v-avatar
-                img(src='/icons/google.svg')
-              v-avatar
-                img(src='/icons/facebook.svg')
-              v-avatar
-                img(src='/icons/twitter.svg')
-              v-avatar
-                img(src='/icons/windows.svg')
-  
+    
 </template>
 
 <script>
   export default {
     data: () => ({
-      dialog: false
+      dialog: false,
+      socials: [
+        {icon: '/icons/google.svg'},
+        {icon: '/icons/facebook.svg'},
+        {icon: '/icons/twitter.svg'},
+        {icon: '/icons/windows.svg'}
+      ]
       
       
     }),
