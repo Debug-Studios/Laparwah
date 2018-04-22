e3f4eb946c25fb778dffff3b2ed77cf7c3fcddc5
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-04-22 14:22:17 +0530

    Added padding to user panel in navbar.

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -11,10 +11,10 @@
         v-btn(icon='' slot='activator')
           v-avatar(size='32')
             v-gravatar(v-bind:email='email'  )
-        v-card(style="padding-top:10px;")
-         v-flex.text-xs-center(xs12)
+        v-card(style="margin-top:10px")
+         v-flex.text-xs-center(xs12 style='padding:15px')
           span Welcome! {{name}}
-         v-flex.text-xs-center(xs12)
+         v-flex.text-xs-center(xs12 style='padding:10px')
             a(href='/#/dashboard')
               v-btn(fab small dark)
                 v-icon settings
