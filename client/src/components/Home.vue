3e89d7cfefff180359b705e02cd622bfea460139
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-04-23 15:52:56 +0530

    Added categories

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -2,11 +2,27 @@
   v-app
     navbar
 
-    v-jumbotron
-      v-container
-        v-layout
-          v-flex(text-xs-center)
-            h3.display-3 Laaparwah
+    v-jumbotron(style="height: 20vh;")
+      v-container(fill-height)
+        v-layout(align-center)
+          v-flex
+            h3.display-3.laparwah-font लापरवाह
+
+    v-toolbar(flat)
+      v-toolbar-items
+        v-btn(flat) Regions
+        v-btn(flat) Politics
+        v-btn(flat) Money
+        v-btn(flat) Entertainment
+        v-btn(flat) Tech
+        v-btn(flat) Sport
+        v-btn(flat) Travel
+        v-btn(flat) Style
+        v-btn(flat) Health
+        v-btn(flat) Video
+      v-spacer
+      v-toolbar-items
+        v-text-field(name="search", label="Search", append-icon="search")
 </template>
 
 <script>
@@ -19,3 +35,9 @@ export default {
 };
 </script>
 
+<style lang="scss">
+.laparwah-font {
+  font-family: "Kalam", cursive;
+  font-weight: bolder;
+}
+</style>
