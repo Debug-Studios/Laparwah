aadc28faa01a9470e1e222ca7225d7c61f7bbb70
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-04-25 13:20:43 +0530

    Added axios post method.

--- server/routes/dashboard-routes.js
+++ server/routes/dashboard-routes.js
@@ -134,7 +134,6 @@ router.delete('/deleteNewsPost/:id', IsAdmin, (req, res) => {
 
 // For Editor
 router.post('/createNewsPost', IsEditor, (req, res) => {
-  console.log(req);
   News.create(
     {
       creator_id: req.user._id,
