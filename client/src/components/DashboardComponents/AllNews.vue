4343f16e46f0eaf53cd0ad2331f57c2fb30e6e21
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-04-27 11:00:34 +0530

    Added DeletePost option with animation #4

--- client/src/components/DashboardComponents/AllNews.vue
+++ client/src/components/DashboardComponents/AllNews.vue
@@ -1,23 +1,24 @@
 <template lang="pug">
   #allnews
-    v-card(v-for='(news, index) in allnews' :key='news._id')
-        v-card-title.blue-grey.darken-2.py-4.title
-            | {{news.title}}
-        v-container.pa-4(grid-list-sm )
-            v-layout(row wrap)
-                v-flex(xs6)
-                   h3( label='Category' name='category' ) Category: {{news.category}}
-                v-spacer
-                v-flex(xs3)
-                    h3(name='tag') Tag: {{news.main_tag}}
-                v-flex(xs12)
-                    p(name='show_content' label='Content') {{news.content}}
-                v-flex(xs6)
-                    h3(label='Applied Tags' name='applied_tags') Applied Tags: {{news.tags}}
-            v-card-actions
-                v-spacer
-                v-btn( color='success' @click.native='' ) Edit
-                v-btn( @click='deletePost') Delete
+    transition-group(name="fade" tag="div")
+        v-card(v-for='(news, index) in allnews' :key='news._id')
+            v-card-title.blue-grey.darken-2.py-4.title
+                | {{news.title}}
+            v-container.pa-4(grid-list-sm )
+                v-layout(row wrap)
+                    v-flex(xs6)
+                    h3( label='Category' name='category' ) Category: {{news.category}}
+                    v-spacer
+                    v-flex(xs3)
+                        h3(name='tag') Tag: {{news.main_tag}}
+                    v-flex(xs12)
+                        p(name='show_content' label='Content') {{news.content}}
+                    v-flex(xs6)
+                        h3(label='Applied Tags' name='applied_tags') Applied Tags: {{news.tags}}
+                v-card-actions
+                    v-spacer
+                    v-btn( color='success' @click.native='' ) Edit
+                    v-btn( @click='deletePost(news._id)' ) Delete
 </template>
 <script>
 export default {
@@ -26,18 +27,31 @@ export default {
 
     }),
     mounted(){
-        this.axios.get('/dashboard/allNewsPosts/:page').then(response =>{
+        this.axios.get(`/dashboard/allNewsPosts/1`).then(response =>{
             this.allnews = response.data;
-            console.log(response);
-
         });
     },
     methods: {
-        deletePost(){
-            this.axios.post('/deleteNewsPost/:id').then(function(response){
-                console.log(response);
+        deletePost(id){
+            this.axios.delete(`/dashboard/deleteNewsPost/${id}`).then((response)=>{
+                this.allnews.forEach((news, index) => {
+                    if(news._id == response.data._id){
+                        this.allnews.splice(index, 1);
+                        this.$notify({
+                        group: "dashboard",
+                        title: "Successfully Deleted",
+                        type: "success",
+                        duration: 30000
+                        });
+                    }
+                });
             }).catch(function(error){
-                console.log(error);
+                this.$notify({
+                        group: "dashboard",
+                        title: "Cannot Delete! Try Again",
+                        type: "error",
+                        duration: 30000
+                        });
             })
 
         }
