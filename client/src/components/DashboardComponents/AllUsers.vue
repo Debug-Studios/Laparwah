3aebc6775fec99f0b0c9fb72c94707c90490b656
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-04-28 01:44:06 +0530

    Added Delete user option for Admin

--- client/src/components/DashboardComponents/AllUsers.vue
+++ client/src/components/DashboardComponents/AllUsers.vue
@@ -1,18 +1,20 @@
 <template lang="pug">
-    v-flex(xs4)
-        v-card(v-for='(users, index) in allusers' :key='users._id')
-            v-card-media#media
-                v-flex.text-xs-center
-                    v-avatar(size='180px')
-                        v-gravatar(email='ayush.bahuguna12@gmail.com' size='100')
-            v-divider
-            v-card-title
-                div
-                    h3 {{users.name}}
-                    span Roles: {{users.roles}}
-            v-card-actions
-                v-spacer
-                v-btn(color='success' flat) Remove User
+    #allusers
+        v-flex(xs4)
+            transition-group(name="fade" tag="div")
+                v-card(v-for='(users, index) in allusers' :key='users._id')
+                    v-card-media#media
+                        v-flex.text-xs-center
+                            v-avatar(size='180px')
+                                v-gravatar(email='ayush.bahuguna12@gmail.com')
+                    v-divider
+                    v-card-title
+                        div
+                            h3 {{users.name}}
+                            span Roles: {{users.roles}}
+                    v-card-actions
+                        v-spacer
+                        v-btn(@click='deleteUser(users._id)' color='success' flat ) Remove User
 
 </template>
 <script>
@@ -25,11 +27,35 @@ export default {
             console.log(response);
             this.allusers = response.data;
         });
+    },
+    methods:{
+        deleteUser(id){
+            this.axios.delete(`/dashboard/deleteAccount/${id}`).then((response) => {
+                this.allusers.forEach((users, index)=>{
+                    if(users._id == response.data._id){
+                        this.allusers.splice(index,1);
+                        this.$notify({
+                        group: "dashboard",
+                        title: "Successfuly Deleted",
+                        type: "success",
+                        duration: 30000
+                        });
+                    }
+                })
+            }).catch((error) =>{
+                this.$notify({
+                        group: "dashboard",
+                        title: "Cannot Delete! Try Again",
+                        type: "error",
+                        duration: 30000
+                        });
+            })
+        }
+
     }
 }
 </script>
 <style scoped>
-    
     #media{
         padding: 15px;
     }
