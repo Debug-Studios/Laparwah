47e19ac6623906c1b66835759671e05e40c6a9c3
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-04-29 01:12:21 +0530

    Added router-link to news posts on Home #16

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -28,61 +28,66 @@
     v-layout(column)
       v-flex(xs12 sm12)
         v-card
-          v-container(fluid grid-list-xl)
+          v-container(fluid grid-list-md)
             v-layout(row wrap).mt-1
               v-flex(xs12 md8 style="display: flex;" v-if="breakingNews[0]")
-                v-jumbotron(:src="breakingNews[0].heroImage" dark :gradient="gradients[0]" height="100%")
-                  v-container(fill-height)
-                      v-layout(fill-height)
-                        v-flex.breaking-flex(xs12 align-end flexbox dark)
-                          v-chip(label color="red" text-color="white") BREAKING NEWS
-                          v-flex.breaking-flex--text
-                            .display-2(style="font-weight: 800") {{breakingNews[0].title}}
-                            span.subheading.mt-2.grey--text.text--lighten-1.ml-1
-                              span by {{breakingNews[0].creator.name}}
-                              v-icon(left color="grey").mr-1.ml-2 schedule
-                              span {{breakingNews[0].created_at | moment("dddd, MMMM Do YYYY")}}
+                router-link(:to="'/news/' + breakingNews[0]._id").plain
+                  v-jumbotron(:src="breakingNews[0].heroImage" dark :gradient="gradients[0]" height="100%")
+                    v-container(fill-height)
+                        v-layout(fill-height)
+                          v-flex.breaking-flex(xs12 align-end flexbox dark)
+                            v-chip(label color="red" text-color="white") BREAKING NEWS
+                            v-flex.breaking-flex--text
+                              .display-2(style="font-weight: 800") {{breakingNews[0].title}}
+                              span.subheading.mt-2.grey--text.text--lighten-1.ml-1
+                                span by {{breakingNews[0].creator.name}}
+                                v-icon(left color="grey").mr-1.ml-2 schedule
+                                span {{breakingNews[0].created_at | moment("dddd, MMMM Do YYYY")}}
               v-flex(xs12 md4)
                 v-flex(xs12 style="display: flex;" v-if="breakingNews[1]").pt-0.pr-0
-                  v-jumbotron(:src="breakingNews[1].heroImage" dark :gradient="gradients[1]" style="height:20rem")
-                    v-container(fill-height)
-                        v-layout(fill-height)
-                          v-flex.breaking-flex(xs12 align-end flexbox dark)
-                            v-chip(label color="red" text-color="white") BREAKING NEWS
-                            v-flex.breaking-flex--text
-                              .headline(style="font-weight: 800") {{breakingNews[1].title}}
-                              span.body-2.mt-2.grey--text.text--lighten-1.ml-1
-                                span by {{breakingNews[1].creator.name}}
-                                v-icon(left color="grey").mr-1.ml-2 schedule
-                                span {{breakingNews[1].created_at | moment("dddd, MMMM Do YYYY")}}
+                  router-link(:to="'/news/' + breakingNews[1]._id").plain
+                    v-jumbotron(:src="breakingNews[1].heroImage" dark :gradient="gradients[1]" style="height:20rem")
+                      v-container(fill-height)
+                          v-layout(fill-height)
+                            v-flex.breaking-flex(xs12 align-end flexbox dark)
+                              v-chip(label color="red" text-color="white") BREAKING NEWS
+                              v-flex.breaking-flex--text
+                                .headline(style="font-weight: 800") {{breakingNews[1].title}}
+                                span.body-2.mt-2.grey--text.text--lighten-1.ml-1
+                                  span by {{breakingNews[1].creator.name}}
+                                  v-icon(left color="grey").mr-1.ml-2 schedule
+                                  span {{breakingNews[1].created_at | moment("dddd, MMMM Do YYYY")}}
                 v-flex(xs12 style="display: flex;" v-if="breakingNews[2]").pb-0.pr-0
-                  v-jumbotron(:src="breakingNews[2].heroImage" dark :gradient="gradients[1]"  style="height:20rem")
-                    v-container(fill-height)
-                        v-layout(fill-height)
-                          v-flex.breaking-flex(xs12 align-end flexbox dark)
-                            v-chip(label color="red" text-color="white") BREAKING NEWS
-                            v-flex.breaking-flex--text
-                              .headline(style="font-weight: 800") {{breakingNews[2].title}}
-                              span.body-2.mt-2.grey--text.text--lighten-1.ml-1
-                                span by {{breakingNews[2].creator.name}}
-                                v-icon(left color="grey").mr-1.ml-2 schedule
-                                span {{breakingNews[2].created_at | moment("dddd, MMMM Do YYYY")}}
+                  router-link(:to="'/news/' + breakingNews[2]._id").plain
+                    v-jumbotron(:src="breakingNews[2].heroImage" dark :gradient="gradients[1]"  style="height:20rem")
+                      v-container(fill-height)
+                          v-layout(fill-height)
+                            v-flex.breaking-flex(xs12 align-end flexbox dark)
+                              v-chip(label color="red" text-color="white") BREAKING NEWS
+                                .headline(style="font-weight: 800") {{breakingNews[2].title}}
+                              v-flex.breaking-flex--text
+                                span.body-2.mt-2.grey--text.text--lighten-1.ml-1
+                                  span by {{breakingNews[2].creator.name}}
+                                  v-icon(left color="grey").mr-1.ml-2 schedule
+                                  span {{breakingNews[2].created_at | moment("dddd, MMMM Do YYYY")}}
 
-            v-layout(row).px-5.pt-5
+          v-container(fluid grid-list-xl)
+            v-layout(row).px-5
               v-flex(xs12 sm12)
                 h3.headline(style="font-weight: 800").primary--text SPOTLIGHT
                 .py-4(fluid grid-list-lg)
                   v-layout(row wrap)
                     v-flex(xs12 md3 v-for="(news, index) in spotlights" :key="news._id")
-                      v-card(flat)
-                        v-card-media(:src="news.heroImage" height="10rem" cover)
-                        v-card-title(primary-title).px-0
-                          div
-                            h3.title.ml-1.mb-2 {{news.title}}
-                            span.body-2.mt-2.grey--text.text--lighten-1.ml-1
-                              span by {{news.creator.name}}
-                              v-icon(left color="grey").mr-1.ml-2 schedule
-                              span {{news.created_at | moment("from", "now")}}
+                      router-link(:to="'/news/' + news._id").plain
+                        v-card(flat)
+                          v-card-media(:src="news.heroImage" height="10rem" cover)
+                          v-card-title(primary-title).px-0
+                            div
+                              h3.title.ml-1.mb-2 {{news.title}}
+                              span.body-2.mt-2.grey--text.text--lighten-1.ml-1
+                                span by {{news.creator.name}}
+                                v-icon(left color="grey").mr-1.ml-2 schedule
+                                span {{news.created_at | moment("from", "now")}}
 
             v-layout(row wrap grid-list-xl).px-5
               v-flex(md4 sm12 v-for="(category, index1) in newsCategories" :key="index1")
@@ -90,15 +95,16 @@
                 div(v-if="newsCategoriesData" v-for="(newsPosts, index2) in newsCategoriesData" :key="index2")
                   div(xs12 style="display: flex;" v-if="index2 == index1" v-for="(news, index3) in newsPosts" :key="news._id")
                     v-jumbotron.py-4(:src="news.heroImage" dark :gradient="gradients[1]" height="16rem" v-if="index3 == 0")
-                      v-container(fill-height)
-                          v-layout(fill-height)
-                            v-flex.breaking-flex(xs12 align-end flexbox dark).pb-0
-                              v-chip(label color="red" text-color="white") {{news.category}}
-                              v-flex.breaking-flex--text
-                                .title(style="font-weight: 800") {{news.title}}
-                                span.body-2.grey--text.text--lighten-1.pb-0
-                                  span {{news.created_at | moment("from", "now")}}
-                    a.plain.pb-2(v-else style="width: 100%" href="#").pt-3
+                      router-link(:to="'/news/' + news._id").plain
+                        v-container(fill-height)
+                            v-layout(fill-height)
+                              v-flex.breaking-flex(xs12 align-end flexbox dark).pb-0
+                                v-chip(label color="red" text-color="white") {{news.category}}
+                                v-flex.breaking-flex--text
+                                  .title(style="font-weight: 800") {{news.title}}
+                                  span.body-2.grey--text.text--lighten-1.pb-0
+                                    span {{news.created_at | moment("from", "now")}}
+                    router-link(:to="'/news/' + news._id" v-else style="width: 100%").pt-3.plain.pb-2
                       v-layout(grid row style="align-items: center").pb-3.mx-0
                         .red--text.mr-2(xs1 style="font-size: 6px;") &#9679;
                         .body-2.black--text.grey--text.text--darken-3(xs11 style="font-weight: 400") {{news.title}}
