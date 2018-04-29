34a2e66611f8740ba9a282028009d8a460ace610
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-04-29 20:32:59 +0530

    Added social links to footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,16 +1,32 @@
 <template lang="pug">
   v-footer(height="auto" dark)
     v-layout(row wrap justify-center)
-      v-btn(color="white" flat)
-        router-link(:to="'/'").plain Home
-      v-btn(color="white" flat)
-        router-link(:to="'/about'").plain About Us
-      v-btn(color="white" flat)
-        router-link(:to="'/team'").plain Team
+      v-flex(xs12 pt-2 pb-0 text-xs-center white--text).grey.darken-3
+        v-btn(fab flat small)
+          v-avatar(size="32")
+            img(src='/icons/facebook.svg')
+        v-btn(fab flat small)
+          v-avatar(size="32")
+            img(src='/icons/twitter.svg')
 
-      v-flex(xs12 py-3 text-xs-center white--text)
+      v-flex(xs12 pb-2 text-xs-center white--text).grey.darken-3
+        v-btn(color="white" flat)
+          router-link(:to="'/'").plain Home
+        v-btn(color="white" flat)
+          router-link(:to="'/about'").plain About Us
+        v-btn(color="white" flat)
+          router-link(:to="'/team'").plain Team
+        v-btn(color="white" flat)
+          router-link(:to="'/team'").plain Contact-Us
+
+      v-flex(xs12 py-4 text-xs-center white--text).black
+
         | &copy;2018 —&nbsp;
-        strong Laparwah and Debug Studios
+        | Made with&nbsp;
+        b(style="color:#f42") ♥&nbsp;
+        | by&nbsp;
+        strong Debug Studios
+
 </template>
 
 <script>
