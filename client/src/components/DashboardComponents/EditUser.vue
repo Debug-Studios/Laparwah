1eade25cc9a18825d902276d7947120687ac5cc5
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-05-01 01:39:16 +0530

    Added toggle button for role in edit user page

--- client/src/components/DashboardComponents/EditUser.vue
+++ client/src/components/DashboardComponents/EditUser.vue
@@ -7,28 +7,36 @@
                         v-card-media#media
                             v-flex.text-xs-center
                                 v-avatar(size='180px')
-                                    v-gravatar(:email= 'userDetails.email' :size='200')
+                                    v-gravatar(:email= 'acc.email' :size='200')
                         v-divider
                         v-card-title
-                            div
-                                h3 {{userDetails.name}}
-                                h4 Email: {{userDetails.email}}
+                            v-flex(xs12)
+                                h3 Name:{{acc.name}}
+                                h4 Email: {{acc.email}}
+                            v-flex(xs12)
+                                v-switch(label='Admin' v-model='role' value='admin')
+                                v-switch(label='Writer' v-model='role' value='writer')
+                                v-switch(label='Editor' v-model='role' value='editor')
+                                v-switch(label='User' v-model='role' value='user')
+
                         v-card-actions
                             v-spacer
-                            v-btn(@click='' color='success' flat ) Edit
-                            v-btn(@click='' color='success' flat ) Remove User
+                            v-btn(@click='' color='success' flat ) Save
+                            v-btn(@click='' color='red' flat ) Cancel
 
 </template>
 <script>
 export default {
   data: () => ({
-      userDetails: [],
+      acc: {},
+      role: ['user'],
       id: ''
   }),
   mounted(){
+      console.log(this.role);
       this.axios.get(`/dashboard/editAccount/${id}`).then((response) => {
           console.log(response);
-          this.userDetails = response.data;
+          this.acc = response.data;
       });
   }
 }
