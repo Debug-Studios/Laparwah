9a5c14b9b6d7d38e43e31e334ed93e6d0e7efbe4
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-05-02 13:45:08 +0530

    Made main_tag and category field required

--- server/models/news.js
+++ server/models/news.js
@@ -10,8 +10,8 @@ const newsSchema = new Schema(
     heroImage: String,
     locale: String,
     category: { type: String, required: true },
-    main_tag: String,
-    tags: [String],
+    main_tag: {type: String, required: true},
+    tags: {type: [String], required: true},
     approval: {
       done: {
         mod1: { type: Boolean, default: false },
