40b54569ed45df30d380a9b5c621dcc84b92e99f
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-05-03 20:42:56 +0530

    Added webpack prod config

---
+++ client/build/webpack.config.prod.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: './client/src/app.js',
+    dashboard: './client/src/dashboard.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['absolute/path/a', 'absolute/path/b']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'client/index.html',
+      template: 'client/index.html',
+      chunks: ['app'],
+      inject: false
+    }),
+
+    new HtmlWebpackPlugin({
+      filename: 'client/dashboard.html',
+      template: 'client/dashboard.html',
+      chunks: ['dashboard'],
+      inject: true
+    })
+  ]
+};
